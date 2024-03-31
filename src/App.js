12f5198c2bee6012fd42546108f0cf7f0b80eeca
Burakkturk <burak1845turk@icloud.com>
2024-03-31 03:20:23 +0300

    navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.scss";
+import Navbar from "./components/navbar/Navbar";
 import Header from "./components/header/Header";
+
 
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <Header/>
+      
     </div>
   );
 }
---
+++ src/components/main/Card.jsx
@@ -0,0 +1,20 @@
+const Card = (props) => {
+    console.log("ne geliyor", props);
+    return(
+        <div className="cards">
+            <div className="title">
+                <h1>props.item.title</h1>
+            </div>
+            <div className="date">
+                <h2>date</h2>
+            </div>
+            <img src="" alt=""/>
+            <div className="card-over">
+                <p>description</p>
+            </div>
+
+        </div>
+    )
+}
+
+export default Card;
